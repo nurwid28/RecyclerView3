cf9a8ee563dec670d91d9cd95e950eb7ec272a71
Widya <nur_astutik_24rpl@student.smktelkom-mlg.sch.id>
2017-01-09 13:22:47 +0700

    RecyclerView View Detail

--- app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview3/adapter/HotelAdapter.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview3/adapter/HotelAdapter.java
@@ -1,5 +1,8 @@
 package id.sch.smktelkom_mlg.learn.recyclerview3.adapter;
 
+
+import android.content.Context;
+import android.net.Uri;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,16 +10,21 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+
 import java.util.ArrayList;
+
 
 import id.sch.smktelkom_mlg.learn.recyclerview3.R;
 import id.sch.smktelkom_mlg.learn.recyclerview3.model.Hotel;
 
-/**
- * Created by Widya on 04-Nov-16.
- */
 
-public class HotelAdapter extends RecyclerView.Adapter<HotelAdapter.ViewHolder> {
+/**
+ * Created by Dhea on 11/3/2016.
+ */
+public class HotelAdapter extends RecyclerView.Adapter<HotelAdapter.ViewHolder>
+{
+
+
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_list,parent,false);
@@ -24,42 +32,70 @@ public class HotelAdapter extends RecyclerView.Adapter<HotelAdapter.ViewHolder> 
         return vh;
     }
 
+
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
+
+
         Hotel hotel = hotelList.get(position);
         holder.tvJudul.setText(hotel.judul);
         holder.tvDeskripsi.setText(hotel.deskripsi);
-        holder.ivFoto.setImageDrawable(hotel.foto);
+        holder.ivFoto.setImageURI(Uri.parse(hotel.foto));
     }
+
 
     @Override
     public int getItemCount() {
-
-        if(hotelList != null)
+        if(hotelList!=null)
             return hotelList.size();
-
         return 0;
     }
 
+
     public class ViewHolder extends RecyclerView.ViewHolder {
+
 
         ImageView ivFoto;
         TextView tvJudul;
         TextView tvDeskripsi;
+
 
         public ViewHolder(View itemView) {
             super(itemView);
             ivFoto = (ImageView) itemView.findViewById(R.id.imageView);
             tvJudul = (TextView) itemView.findViewById(R.id.textViewJudul);
             tvDeskripsi = (TextView) itemView.findViewById(R.id.textViewDeskripsi);
+
+
+            itemView.setOnClickListener(new View.OnClickListener()
+            {
+                @Override
+                public void onClick(View v)
+                {
+                    mIHotelAdapter.doClick(getAdapterPosition());
+                }
+            });
         }
+
+
+
+
     }
+
 
     ArrayList<Hotel> hotelList;
-
-    public HotelAdapter(ArrayList<Hotel> hotelList)
+    public HotelAdapter(Context context, ArrayList<Hotel> hotelList)
     {
         this.hotelList = hotelList;
+        mIHotelAdapter = (IHotelAdapter) context;
     }
+
+
+    IHotelAdapter mIHotelAdapter;
+    public interface IHotelAdapter
+    {
+        void doClick(int pos);
+    }
+
 
 }
