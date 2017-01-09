7f78fb23813a817aa2144a19fa7475356cf12603
Widya <nur_astutik_24rpl@student.smktelkom-mlg.sch.id>
2017-01-09 08:54:13 +0700

    RecyclerView List Card

---
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview3/model/Hotel.java
@@ -0,0 +1,22 @@
+package id.sch.smktelkom_mlg.learn.recyclerview3.model;
+
+import android.graphics.drawable.Drawable;
+
+/**
+ * Created by Widya on 04-Nov-16.
+ */
+
+public class Hotel {
+
+    public String judul;
+    public String deskripsi;
+    public Drawable foto;
+
+    public Hotel(String judul, String deskripsi, Drawable foto)
+    {
+        this.judul = judul;
+        this.deskripsi = deskripsi;
+        this.foto = foto;
+    }
+
+}
